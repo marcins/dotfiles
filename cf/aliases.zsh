f30dc14d67b4c354093f9d3426985ef7e14cd51d
Marcin Szczepanski <marcins@webqem.com>
2014-04-22 10:32:12 +1000

    Add CF10 Start/Stop/Log aliases

---
+++ cf/aliases.zsh
@@ -0,0 +1,3 @@
+alias cfstart='mysql.server start && /Applications/ColdFusion10/cfusion/bin/cf-standalone-startup start'
+alias cfstop='/Applications/ColdFusion10/cfusion/bin/cf-standalone-startup stop'
+alias cflog='tail -f /Applications/ColdFusion10/cfusion/logs/coldfusion-out.log'
