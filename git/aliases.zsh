d186b68bad4a0ecac8549e81c21be1ebb9ac4c61
Marcin Szczepanski <marcins@webqem.com>
2014-04-22 10:32:00 +1000

    Add 'ga' alias for git add

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,6 +9,7 @@ fi
 alias g='git'
 
 # The rest of my fun git aliases
+alias ga='git add'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
