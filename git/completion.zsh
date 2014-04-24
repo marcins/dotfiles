b01174c7abac45d2bd404f65e79a08978a24876b
Marcin Szczepanski <marcins@webqem.com>
2014-04-24 15:13:27 +1000

    Try to get git flow completion working

--- git/completion.zsh
+++ git/completion.zsh
@@ -5,6 +5,13 @@ completion='$(brew --prefix)/share/zsh/site-functions/_git'
 
 if test -f $completion
 then
+echo 'doing it'
   source $completion
 fi
 compdef g='git'
+
+gf_completion='$(brew --prefix)/share/zsh/site-functions/git-flow-completion.zsh'
+if test -f $gf_completion
+then
+  source $gf_completion
+fi
